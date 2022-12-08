16a284cdcfe5d77ed0e03ced54e95f4676913666
Vladislav Patrakov <gostreid1@gmail.com>
2022-12-08 12:05:48 +0300

    develop +

--- Basics.playground/Pages/6 String Home Work.xcplaygroundpage/Contents.swift
+++ Basics.playground/Pages/6 String Home Work.xcplaygroundpage/Contents.swift
@@ -55,7 +55,7 @@ let str1 = "123"
 let str2 = "3535d"
 let str3 = "54"
 let str4 = "r45"
-let str5 = "56"
+let str5 = "0"
 
 let int1 = Int(str1) ?? 0
 let int2 = Int(str2) ?? 0
@@ -63,21 +63,24 @@ let int3 = Int(str3) ?? 0
 let int4 = Int(str4) ?? 0
 let int5 = Int(str5) ?? 0
 
-let strInt1 = int1 != 0 ? str1 : "nil"
-let strInt2 = int2 != 0 ? str2 : "nil"
-let strInt3 = int3 != 0 ? str3 : "nil"
-let strInt4 = int4 != 0 ? str4 : "nil"
-let strInt5 = int5 != 0 ? str5 : "nil"
+let strInt1 = Int(str1) != nil ? str1 : "nil"
+let strInt2 = Int(str2) != nil ? str2 : "nil"
+let strInt3 = Int(str3) != nil ? str3 : "nil"
+let strInt4 = Int(str4) != nil ? str4 : "nil"
+let strInt5 = Int(str5) != nil ? str5 : "nil"
 
 let sum = int1 + int2 + int3 + int4 + int5
 
 print("\(strInt1) + \(strInt2) + \(strInt3) + \(strInt4) + \(strInt5) = \(sum)")
 var strSum = String()
 var arrayStrSum = [strInt1, strInt2, strInt3, strInt4, strInt5]
+var countArray = arrayStrSum.count
+var total = 0
 
 for str in arrayStrSum {
     strSum += str
-    if str != strInt5 {
+    total += 1
+    if countArray != total {
         strSum += " + "
     } else {
         strSum += " = "
