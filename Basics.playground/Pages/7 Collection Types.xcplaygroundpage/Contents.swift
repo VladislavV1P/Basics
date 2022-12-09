9b7ddf0c2732554535fc3dcf1649fa999e0914a6
Vladislav Patrakov <gostreid1@gmail.com>
2022-12-09 12:29:17 +0300

    1007 Collection Types 3

--- Basics.playground/Pages/7 Collection Types.xcplaygroundpage/Contents.swift
+++ Basics.playground/Pages/7 Collection Types.xcplaygroundpage/Contents.swift
@@ -10,5 +10,11 @@ var arrayOne: [Int] = []
 var arrayTwo = Array<Int>()
 var arrayThree = [Int]()
 var arrayFour = [1, 2, 3]
-var arrayFive = [Int]
+var arrayFive = [Int](repeating: 10, count: 6)
+var arraySix = Array(repeating: 11, count: 6)
 
+arrayThree += arrayFive
+arrayThree[1...3] = [15]
+arrayThree
+
+arrayThree.count
