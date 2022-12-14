f3fb439fc1ac6cd0efa42bc74479546747eedfde
Vladislav Patrakov <gostreid1@gmail.com>
2022-12-14 17:02:16 +0300

    1007 Collection Types 6

--- Basics.playground/Pages/7 Collection Type - Set.xcplaygroundpage/Contents.swift
+++ Basics.playground/Pages/7 Collection Type - Set.xcplaygroundpage/Contents.swift
@@ -2,3 +2,25 @@
 
 import Foundation
 
+//Множества - Set
+/*
+ Набор хранит различные значения одного и того же типа в коллекции без определенного порядка. Вы можете использовать набор вместо массива, когда порядок элементов не важен или когда вам нужно убедиться, что элемент появляется только один раз.
+ */
+
+let setOne = Set<String>()
+let setTwo: Set<String> = []
+var setThere: Set = [1, 2, 3, 4]
+
+setThere.insert(5)
+setThere.insert(6)
+setThere.insert(7)
+setThere.sorted()
+
+setThere.isEmpty
+setThere.remove(6)
+setThere.count
+
+let boolValue = setThere.contains(3)
+
+let setFromOneToThere: Set = [1, 2, 3]
+let setFromFourToNine: Set = [4, 5, 6, 7, 8, 9]
